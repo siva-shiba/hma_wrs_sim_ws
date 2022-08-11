a5c454d1452e21dd8deafda51e754bd3ecbf3c6c
akinobu <akinobu1998@gmail.com>
2022-08-12 01:54:45 +0900

    [New] ros_mode (experimental)

--- entrypoints/bash.sh
+++ entrypoints/bash.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+TERM=xterm-color
 source /opt/ros/melodic/setup.bash
 source /home/ros_ws/hma/cv_bridge_ws/install/setup.bash --extend
 source /home/ros_ws/hma/hma_wrs_sim_ws/devel/setup.bash
-bash
+bash --rcfile ~root/.bashrc
+# PS1='${debian_chroot:+($debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\$ '
+# PS1='${debian_chroot:+($debian_chroot)}\[\033[30m\]\[\033[43m\][ros_mode]\[\033[01;49m\] \[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\$ '
