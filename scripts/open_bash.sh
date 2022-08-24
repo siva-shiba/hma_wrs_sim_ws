099d8fbb529fe923f69bad568506bc9d62925b39
akinobu <akinobu1998@gmail.com>
2022-08-24 19:15:55 +0900

    [Mod] Change container name

--- scripts/open_bash.sh
+++ scripts/open_bash.sh
@@ -1,1 +1,1 @@
-docker exec -it --env ROS_MASTER_URI=http://172.18.0.1:11311 --env TERM=xterm-color --workdir /home/ros_ws/hma/hma_wrs_sim_ws hma_wrs_sim_ws-hma_wrs_sim_ws-1 /entrypoints/bash.sh
+docker exec -it --env ROS_MASTER_URI=http://172.18.0.1:11311 --env TERM=xterm-color --workdir /home/ros_ws/hma/hma_wrs_sim_ws hma-hma_wrs_sim_ws-1 /entrypoints/bash.sh
