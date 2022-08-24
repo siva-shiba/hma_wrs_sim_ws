8bf01453625bf14899a3b76c1f4f972acd20b5c9
akinobu <akinobu1998@gmail.com>
2022-08-25 07:15:16 +0900

    [Mod] Colorful terminal

--- entrypoints/build.sh
+++ entrypoints/build.sh
@@ -2,10 +2,11 @@
 
 cd /home/ros_ws/hma/hma_wrs_sim_ws
 catkin build --no-status
-
 source /opt/ros/melodic/setup.bash
 source /home/ros_ws/hma/cv_bridge_ws/install/setup.bash --extend
 source /home/ros_ws/hma/hma_wrs_sim_ws/devel/setup.bash
+
+TERM=xterm-color
 bash --rcfile ~root/.bashrc
 # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\$ '
 # PS1='${debian_chroot:+($debian_chroot)}\[\033[30m\]\[\033[43m\][ros_mode]\[\033[01;49m\] \[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\$ '
