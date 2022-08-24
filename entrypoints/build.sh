a88d74b8aafdc6834e18c819d5409e85842463e1
akinobu <akinobu1998@gmail.com>
2022-08-25 07:45:32 +0900

    [Mod] Change command name and build script

--- entrypoints/build.sh
+++ entrypoints/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd /home/ros_ws/hma/hma_wrs_sim_ws
+source /opt/ros/melodic/setup.bash
+cd /home/ros_ws/hma/hma_wrs_sim_ws
 catkin build --no-status
+
+TERM=xterm-color
 source /opt/ros/melodic/setup.bash
 source /home/ros_ws/hma/cv_bridge_ws/install/setup.bash --extend
 source /home/ros_ws/hma/hma_wrs_sim_ws/devel/setup.bash
-
-TERM=xterm-color
+echo "source /root/.bashrc_hma" >> /root/.bashrc
 bash --rcfile ~root/.bashrc
-# PS1='${debian_chroot:+($debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\$ '
-# PS1='${debian_chroot:+($debian_chroot)}\[\033[30m\]\[\033[43m\][ros_mode]\[\033[01;49m\] \[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;35m\]\w\[\033[00m\]\$ '
--- scripts/start_roscore.sh
+++
@@ -1,1 +0,0 @@
-docker exec -it --env ROS_MASTER_URI=http://172.18.0.1:11311 hma_wrs_sim_ws-hma_wrs_sim_ws-1 /entrypoints/roscore.sh
